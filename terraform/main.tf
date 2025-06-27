8879dd4b35ed79689b2268da45e0514b518e68cc
cclaurevju <Carlos.Claure@jala.university>
2025-06-27 17:55:33 -0400

    add terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,48 @@
 provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
+  name     = "my-resource-group"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
+  name                = "my-virtual-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
+  name                = "my-network-interface"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "my-ip-config"
+    subnet_id                    = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_public_ip" "pip" {
+  name                = "my-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
 }
 
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "cclaurevju-vm"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = "ES-US"
+  location            = "East US"
   size                = "Standard_DS1_v2"
   admin_username      = "cclaurevju"
   admin_password      = "cclaurevjupass123"
